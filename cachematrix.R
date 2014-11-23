6a796cbd4a3bb90fd69e3f928df580f3a6130008
L-Roman <lidia.roman.gonzalez@gmail.com>
2014-11-23 17:41:30 -0500

    Revert "Programming-Assignment-2"
    This reverts commit a476086f80187cec0c3c96e0111e5858a44ceb62.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-makeCacheMatrix <- function(x = matrix()) { # makeCacheMatrix creates a matrix, which is really a list that contains a function to set and get the inverse of the matrix
-        m <- NULL                           
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) { # makeCacheMatrix creates a matrix, 
              getinverse = getinverse)
 }
 
-cacheSolve <- function(x, ...) {  # cacheSolve calculates the inverse of the matrix created with the above function. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the setinverse function.
+cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         if(!is.null(m)) {
         message("getting cached data")
